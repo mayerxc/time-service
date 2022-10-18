1ade82b810f2fbff58f5467b50306bf924e81e26
Chris Mayer <mayerxc@gmail.com>
2022-10-18 10:36:18 -0400

    Cleanup

--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,52 @@
 import express from 'express';
 
+function unixToNatural(unix) {
+  const date = new Date(unix * 1000);
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const month = months[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const result = `${month} ${day}, ${year}`;
+  return result;
+}
+
 const router = express.Router();
 
 router.get('/:time', (req, res) => {
-  function unixToNatural(unix) {
-    const date = new Date(unix * 1000);
-    const months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ];
-    const month = months[date.getMonth()];
-    const day = date.getDate();
-    const year = date.getFullYear();
-    const result = `${month} ${day}, ${year}`;
-    return result;
-  }
-
   if (!isNaN(req.params.time)) {
     // if is a number that can be unix time
-    const result = unixToNatural(req.params.time);
-    const data = { unix: req.params.time, natural: result };
-    res.json(data);
+    const natural = unixToNatural(req.params.time);
+    const result = { unix: req.params.time, natural: natural };
+    res.json(result);
   } else {
     // if NaN then try to turn it into a date
-    const natural = new Date(req.params.time);
-    if (!isNaN(natural)) {
+    const date = new Date(req.params.time);
+    if (!isNaN(date)) {
       // did the date turn into a javascript date number???
-      const unix = natural / 1000;
-      const intoNatural = unixToNatural(unix);
+      const unix = date / 1000;
+      const natural = unixToNatural(unix);
 
       // using intoNatural instead of req.params.time because returns whatever date format is there
       // instead of nice unixToNatural format
-      const data3 = { unix: unix, natural: intoNatural };
-      res.json(data3);
+      const result = { unix: unix, natural: natural };
+      res.json(result);
     } else {
       // if it didn't turn into a date format respond with nulls
-      const data2 = { unix: null, natural: null };
-      res.json(data2);
+      const result = { unix: null, natural: null };
+      res.json(result);
     }
   }
 });
